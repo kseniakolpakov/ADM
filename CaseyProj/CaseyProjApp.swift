2fa489e0941ef11ebd355bc0585e7f07ff6a83d3
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-05-16 13:20:53 +0300

    Initial Commit

---
+++ CaseyProj/CaseyProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaseyProjApp.swift
+//  CaseyProj
+//
+//  Created by Yaroslav Monastyrev on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CaseyProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
